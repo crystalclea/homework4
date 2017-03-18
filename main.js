8e024014c97c31388a0d3efdf2feba2b61e32e38
crystalclea <14219182@life.hkbu.edu.hk>
2017-03-18 17:17:39 +0800

    Update and rename main.js to jquery.html

--- main.js
+++
@@ -1,8 +0,0 @@
-var customName = document.getElementById('customname');
-var randomize = document.querySelector('.randomize');
-var story = document.querySelector('.story');
-
-function randomValueFromArray(array){
-  return array[Math.floor(Math.random()*array.length)];
-}
-
